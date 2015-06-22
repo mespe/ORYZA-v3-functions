f8c2c292f8eb7cc52fe711cb8235bab33d08b413
Matt E <mespe@ucdavis.edu>
2015-06-22 11:27:10 -0500

    Partial script for working with S.A. weather data

--- Roryza/R/get_current.R
+++ Roryza/R/get_current.R
@@ -3,6 +3,20 @@
 ## M. Espe
 ## May 2015
 
+##' Get the current files being referred to in an Oryza(v3) control file
+##'
+##' This is a convienence function that reads the Oryza(v3)
+##'    control file specified and prints the current active files
+##'    to the console.
+##'
+##' @title Get current Oryza settings
+##' @param file_path complete file path to control file, including
+##'     the name of the file to be read.
+##'
+##' @return active files being referred to in control file
+##'
+##' @author Matthew Espe
+##'
 get_current_oryza <-
   function(file_path)
   {
