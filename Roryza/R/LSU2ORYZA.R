0dec7f9cb5b8ad5742bdc8ca4e2f1d80182b7bcd
Matt E <mespe@ucdavis.edu>
2015-06-30 10:33:28 -0700

    Beginnings of re-organization of package

--- Roryza/R/LSU2ORYZA.R
+++ Roryza/R/LSU2ORYZA.R
@@ -20,7 +20,7 @@
 
 LSU2ORYZA <- function(Crowley = TRUE, year, station_nbr, prefix = 'usla')
 {
-  
+
   x <- all_date_split[[which(as.numeric(names(all_date_split)) == year)]]
   if(Crowley){
     lat = 30.2
@@ -29,17 +29,17 @@ LSU2ORYZA <- function(Crowley = TRUE, year, station_nbr, prefix = 'usla')
     lat = 32.0
     long = -91.7
   }
-  
+
   pwr <- getPOWER(lat, long, ys = year, ye = year)
-  
+
   pwr_idx <- match(as.numeric(x$julian), pwr$weday)
   x_idx <- match(pwr$weday, as.numeric(x$julian), nomatch = 0)
-  
+
   pwr$tmax[pwr_idx] <- round((x$maxairtempf[x_idx] - 32) * (5/9), 2)
   pwr$tmin[pwr_idx] <- round((x$minairtempf[x_idx] - 32) * (5/9), 2)
   pwr$rain[pwr_idx] <- round((x$rainin[x_idx] * 2.54), 2)
   pwr$wind[pwr_idx] <- round((x$avgwindspeedmph[x_idx] * 0.44704), 2)
-  
+
   tmp <- data.frame(
     station_nbr = station_nbr,
     year = pwr$weyr,
@@ -51,7 +51,7 @@ LSU2ORYZA <- function(Crowley = TRUE, year, station_nbr, prefix = 'usla')
     vappre = round((6.11 * 10^((7.5 * pwr$tdew)/(237.3 + pwr$tdew)))/10, 2),
     wind = pwr$wind,
     precip = pwr$rain)
-  
+
   file_name <- paste0(prefix, station_nbr, '.', gsub('^[1|2]', '', year))
   ff <- file(file_name, 'w')
   on.exit(close(ff))
@@ -61,13 +61,13 @@ LSU2ORYZA <- function(Crowley = TRUE, year, station_nbr, prefix = 'usla')
   write.table(tmp, ff, row.names = FALSE, col.names = FALSE,
               quote = FALSE,
               eol = '\r\n', sep =",")
-  
+
 }
 
 createLSUweather <- function(lat, long, x, station_nbr){
   PWR <- getPOWER(lat, long, ys = x$year,
                   ye = x$year)
-  
+
   tmp <- data.frame(
     station_nbr = station_nbr,
     year = x$year,
@@ -81,5 +81,5 @@ createLSUweather <- function(lat, long, x, station_nbr){
     wind = round(as.numeric(MS_data$'WindRun(miles/day)')/24, 2),
     precip = round(as.numeric(MS_data$'Precipitation(inches)') * 2.54, 2),
     stringsAsFactors = FALSE)
-  
+
 }
