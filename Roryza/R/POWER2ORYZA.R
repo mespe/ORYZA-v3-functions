3d90953ec39f75b5c9532ab387af9f9979fa8917
Matt E <mespe@ucdavis.edu>
2015-07-01 09:15:08 -0700

    Commit macs changes

--- Roryza/R/POWER2ORYZA.R
+++ Roryza/R/POWER2ORYZA.R
@@ -64,7 +64,7 @@ getPOWER <- function(lat, lon, ms = 1, ds = 1, ys,
 ##'
 ##' @author Matthew Espe
 ##'
-POWER2ORYZA <- function(year, station_nbr, lat, long, prefix){
+POWER2ORYZA <- function(year, station_nbr, lat, long){
 
   POWER_data <- getPOWER(lat = lat, lon = long, ys = year, ye = year)
 
@@ -81,17 +81,19 @@ POWER2ORYZA <- function(year, station_nbr, lat, long, prefix){
     wind = POWER_data$wind,
     precip = POWER_data$rain)
 
-  file_name <- paste0(prefix, station_nbr, '.', gsub('^[1|2]', '', year))
-  ff <- file(file_name, 'w')
-  on.exit(close(ff))
-  writeLines('* NASA POWER climate data formatted for ORYZA(v3)', ff)
-  writeLines(paste(long, lat, 0,0,0, sep = ','), ff)
-  write.table(tmp, ff, row.names = FALSE, col.names = FALSE,
-              quote = FALSE,
-              eol = '\r\n', sep =",")
-
-
+  ## file_name <- paste0(prefix, station_nbr, '.', gsub('^[1|2]', '', year))
+  ## ff <- file(file_name, 'w')
+  ## on.exit(close(ff))
+  ## writeLines('* NASA POWER climate data formatted for ORYZA(v3)', ff)
+  ## writeLines(paste(long, lat, 0,0,0, sep = ','), ff)
+  ## write.table(tmp, ff, row.names = FALSE, col.names = FALSE,
+  ##             quote = FALSE,
+  ##             eol = '\r\n', sep =",")
+  return(tmp)
 }
+
+
+
 
 # Test functions
 #getwd()
